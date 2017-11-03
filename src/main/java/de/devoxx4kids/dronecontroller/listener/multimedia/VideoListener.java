5e9da5843f6a188b6cdfca2e3e01a4f6ed888ab4
Lastrik <lastrik@protonmail.com>
2017-11-03 15:27:35 +0100

    Changed how video is used
    This commit changes how the video is transmitted and makes it a lot faster by stopping to write the files on the disk and then reading them again

--- src/main/java/de/devoxx4kids/dronecontroller/listener/multimedia/VideoListener.java
+++ src/main/java/de/devoxx4kids/dronecontroller/listener/multimedia/VideoListener.java
@@ -15,87 +15,74 @@ import java.math.MathContext;
 import java.math.RoundingMode;
 
 import static de.devoxx4kids.dronecontroller.command.PacketType.DATA_LOW_LATENCY;
-
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.function.Consumer;
+import java.util.logging.Level;
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
 
 /**
- * <p>Wide angle - 640x480px, 15 frames per second</p>
+ * <p>
+ * Wide angle - 640x480px, 15 frames per second</p>
  *
- * <p>Consumes the Packet:</p>
+ * <p>
+ * Consumes the Packet:</p>
  *
- * <p>[3, 125, sequenceNumber, x, x, x, x, x, x, x, x, x, {Image Starting with -1 (FF), -40 (D8) }</p>
+ * <p>
+ * [3, 125, sequenceNumber, x, x, x, x, x, x, x, x, x, {Image Starting with -1
+ * (FF), -40 (D8) }</p>
  *
- * @author  Tobias Schneider
+ * @author Tobias Schneider
  */
 public class VideoListener implements EventListener {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+	private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    private static final String FRAME_JPG = "frame.jpg";
-    private static byte[] lastJpeg = null;
-    private boolean writeToDisk=true;
-    private float frameRate = 0;
-    long lastFrame = System.currentTimeMillis()-20;
-    MovingAverage average = new MovingAverage(20);
+	private final Consumer<BufferedImage> consumer;
 
-    private VideoListener() {
+	private static byte[] lastJpeg = null;
 
-        // private, please use fabric method
-    }
+	private VideoListener(Consumer<BufferedImage> consumer) {
+		this.consumer = consumer;
+		// private, please use fabric method
+	}
 
-    public static VideoListener videoListener() {
+	public static VideoListener videoListener(Consumer<BufferedImage> consumer) {
+		return new VideoListener(consumer);
+	}
 
-        return new VideoListener();
-    }
+	@Override
+	public void consume(byte[] data) {
+		
+		LOGGER.debug("consuming video packet");
+		try {
+			consumer.accept(ImageIO.read(new ByteArrayInputStream(getJpeg(data))));
+		} catch (IOException ex) {
+			LOGGER.error("error with the video");
+		}
+	}
 
+	@Override
+	public boolean test(byte[] data) {
 
-    @Override
-    public void consume(byte[] data) {
-        MathContext mc = new MathContext(2, RoundingMode.HALF_UP);
-        average.add(new BigDecimal((System.currentTimeMillis() - lastFrame)).divide(new BigDecimal(1000),mc));
+		boolean jpgStart = data[12] == -1 && data[13] == -40;
 
-        LOGGER.debug("consuming video packet at a framerate of {}", new BigDecimal(1).divide(average.getAverage(),mc));
-        byte[] jpeg = getJpeg(data);
-        if (writeToDisk) {
-            File file = new File(FRAME_JPG);
-            try (FileOutputStream fos = new FileOutputStream(file)) {
-                LOGGER.debug("writing video jpg to " + file.getAbsolutePath());
+		return data[0] == DATA_LOW_LATENCY.toByte() && data[1] == 125 && jpgStart;
+	}
 
-                fos.write(jpeg);
-            } catch (IOException e) {
-                LOGGER.error("Could not generate jpg");
-            }
-        }
-        lastFrame=System.currentTimeMillis();
-    }
+	private byte[] getJpeg(byte[] data) {
 
+		int imageLength = data.length - 12;
 
-    @Override
-    public boolean test(byte[] data) {
+		lastJpeg = new byte[imageLength];
+		System.arraycopy(data, 12, lastJpeg, 0, imageLength);
 
-        boolean jpgStart = data[12] == -1 && data[13] == -40;
+		return lastJpeg;
+	}
 
-        return data[0] == DATA_LOW_LATENCY.toByte() && data[1] == 125 && jpgStart;
-    }
-
-
-    private byte[] getJpeg(byte[] data) {
-
-        int imageLength = data.length - 12;
-
-        lastJpeg = new byte[imageLength];
-        System.arraycopy(data, 12, lastJpeg, 0, imageLength);
-
-        return lastJpeg;
-    }
-
-    public byte[] getLastJpeg() {
-        return lastJpeg;
-    }
-
-    /**
-     * @param enable By default writing is on. If file on disk is not needed, it can be switched off
-     */
-    public void setWriteToDisk(boolean enable) {
-        this.writeToDisk = enable;
-    }
+	public byte[] getLastJpeg() {
+		return lastJpeg;
+	}
 }
